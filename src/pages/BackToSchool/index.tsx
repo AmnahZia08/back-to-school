ed34b3414c2ac80e029df61e190c6560942194a9
AmnahZia08 <amnah.zia08@gmail.com>
2024-05-31 12:46:14 -0700

    chore: adding bagpack and basic animation

---
+++ src/pages/BackToSchool/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./style.module.css";
+import BagPack from "@/components/svgs/BagPack";
+import Pencil from "@/components/svgs/pencil";
+import s from "./style.module.css";
+
+const BackToSchool = () => {
+  const [pencilMoved, setPencilMoved] = useState(false);
+
+  const onClick = () => {
+    setTimeout(() => setPencilMoved(true), 300);
+  };
+
+  return (
+    <div className="w-screen h-screen bg-doodle relative">
+      <div className={s.bagPack}>
+        <BagPack />
+      </div>
+      <div className={pencilMoved ? s.moved : s.pencil} onClick={onClick}>
+        <Pencil />
+      </div>
+    </div>
+  );
+};
+
+export default BackToSchool;
