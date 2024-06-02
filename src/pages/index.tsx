a8df9d2294d0900d4d83a1ea1422a16357029b0f
AmnahZia08 <amnah.zia08@gmail.com>
2024-06-02 02:22:21 -0700

    chore: add background blur on modal open

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,19 +20,26 @@ const Home: React.FC<Props> = ({ isOpen: initialOpen }) => {
   };
 
   return (
-    <div className="w-screen h-dvh bg-doodle flex justify-center items-center">
-      {isOpen && (
-        <Modal
-          isOpen={isOpen}
-          onClose={handleCloseModal}
-          onAction={onModalAction}
-        />
-      )}
-      {!isOpen && (
-        <Button className="mx-8" onClick={handleOpenModal}>
-          Are you ready to get back to school? Click here
-        </Button>
-      )}
+    <div className="w-screen h-dvh bg-doodle">
+      {/* blur the background when the modal is opened */}
+      <div
+        className={`w-screen h-dvh ${
+          isOpen ? "backdrop-blur-[2px]" : ""
+        } flex justify-center items-center`}
+      >
+        {isOpen && (
+          <Modal
+            isOpen={isOpen}
+            onClose={handleCloseModal}
+            onAction={onModalAction}
+          />
+        )}
+        {!isOpen && (
+          <Button className="mx-8" onClick={handleOpenModal}>
+            Are you ready to get back to school? Click here
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
